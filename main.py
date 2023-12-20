e1ad27e1435ac6aa72b8d6dad7db54221bf2007d
Administrador <a@a>
2023-12-20 16:40:53 -0600

    Add multiple lines to pages

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
+lines = 270
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,22, 200,22)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
 
     #Set the footer
     pdf.ln(260)
@@ -25,5 +27,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output("output.pdf")
